f4b6d63422fd23a38d851527ea489c4510074e27
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2025-01-05 20:08:50 +0530

    project-05-01-2025

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from './Customers/components/Navigation'
+
+const App = () => {
+  return (
+    <div >
+      <Navigation/>
+      <h1 className='align-self-end text-red-800'>hi ecommerce</h1>
+    </div>
+  )
+}
+
+export default App
